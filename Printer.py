5ddc8837a4af1edc745ae539de571842d5aa657b
AlexVolk31 <66390806+AlexVolk31@users.noreply.github.com>
2020-06-25 13:53:34 +0300

    Second wave
    More animation and UI

--- Printer.py
+++ Printer.py
@@ -1,24 +1,52 @@
 import time  # Библиотека для работы со временем
 import os  # Библиотека для работы с системой
 
-def getready(place, NoF):
+
+def interface(place):
     mas = []
-    for i in range(NoF):
-        string = place+r'\ASCII-'+str(i)+'.txt'
-        print(place+r'\ASCII-'+str(i)+'.txt')
-        file = open(place+r'\ASCII-'+str(i)+'.txt', 'rt')
+    info = -1
+    print(*os.listdir(place),sep=' <- ', end=' <-\n')
+    while info==-1:
+        try:
+            inputplace = place + '\\' + input('Введите название анимации из списка:\n')
+            info = len(os.listdir(inputplace))
+        except :
+            print("Неверное имя файла")
+    for i in range(info):
+        file = open(inputplace + r'\ASCII-' + str(i) + '.txt', 'rt')
         mas.append(file.read())
-        print(file.read())
     return mas
 
-#C:\Users\AlexVolk\Desktop\animations\sign'
+def animation(frames):
+    while True:
+        for frame in frames:
+            os.system('cls')  # Очистка экрана перед новым кадром
+            print(frame, end='')  # Отрисовка самого кадра
+            time.sleep(1 / (len(frames)+30))  # Задержка для плавносли отрисовки
 
-def main(n):
-    directory = r'animations\sign'
-    frames = getready(directory, 25)
-    for frame in frames * n:
-        os.system('cls')  # Очистка экрана перед новым кадром
-        print(frame, end='')  # Отрисовка самого кадра
-        time.sleep(1 / 60)  # Задержка для плавносли отрисовки
+def getready(place):
+    mas = []
+    print(os.listdir(r'animations'))
+    for i in range(NoF):
+        file = open(place+r'\ASCII-'+str(i)+'.txt', 'rt')
+        mas.append(file.read())
+    return mas
 
-main(1000)
+
+
+def main():
+    directory = 'animations'
+    while True:
+        try:
+            inputframes = interface(directory)
+            print("Для остановки анимации нажмите Ctrl+C")
+            time.sleep(2)
+            animation(inputframes)
+        except KeyboardInterrupt:
+            pass
+
+
+
+
+
+main()
